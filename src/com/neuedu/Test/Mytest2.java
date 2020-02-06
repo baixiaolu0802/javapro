3a568d5e676bebbbea77e2eb1c3d6e8081e04757
baixiaolu0802 <1393002790@qq.com>
2020-02-06 20:02:03 +0800

    新添Mytest2 修改Mytest1

--- src/com/neuedu/Test/Mytest2.java
+++ src/com/neuedu/Test/Mytest2.java
@@ -3,9 +3,8 @@ package com.neuedu.Test;
 /**
  * Created by 13930 on 2020/2/6.
  */
-public class Mytest1 {
+public class Mytest2 {
     public static void main(String[] args) {
-        System.out.println("1111");
-        System.out.println("2222");
+        System.out.println("3333");
     }
 }
