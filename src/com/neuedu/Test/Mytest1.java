61c6e54a0ade052c7052710c8ed5b5276a4d1218
baixiaolu0802 <1393002790@qq.com>
2020-02-06 19:52:25 +0800

    Initial commit

---
+++ src/com/neuedu/Test/Mytest1.java
@@ -0,0 +1,10 @@
+package com.neuedu.Test;
+
+/**
+ * Created by 13930 on 2020/2/6.
+ */
+public class Mytest1 {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
